549a3b1e9a1d1f603c66575a0fb3aeaa4cc197a9
Amatias13 <200221074@estudantes.ips.pt>
2023-04-28 12:55:02 +0100

    final commit

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -7,6 +7,7 @@ public class GroundTile : MonoBehaviour
     public GameObject obstaclePrefab2;
     public GameObject obstaclePrefab3;
     public GameObject obstaclePrefab4;
+    public GameObject obstaclePrefab5;
     public GameObject PowerUpPrefabBoost;
     public GameObject PowerUpPrefabPoints;
 
@@ -28,11 +29,11 @@ public class GroundTile : MonoBehaviour
         {
             if (randomNumber == 0)
             {
-                SpawnObstacle(5, 10, obstaclePrefab);
+                SpawnObstacle(5, 10, obstaclePrefab2);
             }
             else
             {
-                SpawnObstacle(5, 10, obstaclePrefab3);
+                SpawnObstacle(5, 10, obstaclePrefab5);
             }
         }
         else if (currentSkyboxName == "SkyBox-2")
@@ -43,7 +44,7 @@ public class GroundTile : MonoBehaviour
             }
             else
             {
-                SpawnObstacle(5, 10, obstaclePrefab2);
+                SpawnObstacle(5, 10, obstaclePrefab3);
             }
         }
         else if (currentSkyboxName == "SkyBox-3")
@@ -70,10 +71,19 @@ public class GroundTile : MonoBehaviour
 
     void SpawnObstacle(int begin, int end, GameObject a)
     {
-        Debug.Log("SpawnObstacle");
         int obstacleSpawnIndex = Random.Range(begin, end);
         Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
-        Instantiate(a, spawnPoint.position, Quaternion.identity, transform);
+
+        if (a.name == "rock")
+        {
+            Vector3 spawnPosition = new Vector3(spawnPoint.position.x, spawnPoint.position.y - 0.3f, spawnPoint.position.z);
+            Instantiate(a, spawnPosition, Quaternion.identity, transform);
+        }
+        else
+        {
+            Instantiate(a, spawnPoint.position, Quaternion.identity, transform);
+        }
+        
     }
 
     void SpawnPowerUp()
--- Assets/SpawnPortal.cs
+++ Assets/SpawnPortal.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SpawnPortal : MonoBehaviour
 {
     public GameObject PortalPrefab;
+    public GameObject PortalPrefab2;
 
     float timer = 0f;
-    float duration = 10f; // the duration of the timer in seconds
-    float z = -300f;
+    float duration = 15f; // the duration of the timer in seconds
+    float z = -150f;
 
     // Update is called once per frame
     void Update()
@@ -18,14 +19,25 @@ public class SpawnPortal : MonoBehaviour
         if (timer >= duration)
         {
             Spawn();
-            z -= 300f;
+            z -= 150f;
             timer = 0f;
         }
     }
     
     void Spawn()
     {
-        Vector3 spawnPoint = new Vector3(0f, 1.5f, z); // replace this with the desired spawn location
-        Instantiate(PortalPrefab, spawnPoint, Quaternion.Euler(0, 90, 0));
+        int randomNumber = Random.Range(0, 2);
+        
+        if (randomNumber == 0)
+        {
+            Vector3 spawnPoint = new Vector3(0f, 1.5f, z); // replace this with the desired spawn location
+            Instantiate(PortalPrefab, spawnPoint, Quaternion.Euler(0, 90, 0));
+        }
+        else
+        {
+            Vector3 spawnPoint = new Vector3(1.54f, 0.6f, z); // replace this with the desired spawn location
+            Instantiate(PortalPrefab2, spawnPoint, Quaternion.Euler(0, 0, 0));
+        }
+
     }
 }
